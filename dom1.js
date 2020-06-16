cfb8941bcc5c70c00d48bc07a90bac85c80fb641
ajha31 <ajha57775@gmail.com>
2020-06-16 12:51:51 +0530

    made search bug free

--- dom1.js
+++ dom1.js
@@ -1,46 +1,3 @@
-//console.dir(document);
-//console.log(document.URL);
-//console.log(document.title);
-//console.log(document.all[9]);
-//const a=(document.getElementById('header'))
-//console.log(a);
-//a.textContent='hellp'
-//a.innerText='hello'
-//console.log(header.innerText)
-//const a=(document.getElementById('header'));
-//a.innerHTML='<marquee>hello</marquee>';
-//const a=(document.getElementById('main-header'));
-//a.style.backgroundColor='red';
-//ar a=document.querySelector('input');
-//a.value='aman';
-//var a=document.querySelector('input[type="submit"]');
-//var a=document.querySelector('li:last-child');
-//a.innerText='aman';
-//var a=document.querySelector('li:nth-child(3)');
-//a.style.color='red';
-
-//traversing through multiple elements
-
-//var a=document.querySelectorAll('li:nth-child(even)');
-//for (var i =0; i<a.length; i++) {
-//a[i].style.backgroundColor='red';}
-//var a=document.querySelectorAll('li:nth-child(even)');
-//for (var i = 0; i <a.length; i++) {
-//a[i].style.backgroundColor='blue';}
-
-/*
-//creating appending and inserting in dom
-var a=document.createElement('div');
-a.id=('new');
-a.className=('newdiv')
-a.setAttribute('title','titlediv')
-var b=document.createTextNode('hello world');
-a.appendChild(b);
-var c=document.querySelector('.content')
-var d=document.querySelector('h1')
-c.insertBefore(a,d);
-a.style.fontSize='30px'
-console.log(d);*/
 
 
 var a=document.getElementById('button').addEventListener
@@ -75,7 +32,7 @@ function mousemov(e){
 	form.style.backgroundColor='rgb('+e.offsetY+','+e.offsetX+',59)'
 }
 
-
+let lid=[{id:'item'},{id:'item 2'},{id:'item 3'},{id:'item 4'}]
 //actual application
 //adding element
 var a=document.getElementById('submit');
@@ -88,13 +45,15 @@ function add(e){
 	li.setAttribute('class','list')
 	var t=document.createTextNode(c.value);
 	li.appendChild(t);
-	
+	li.setAttribute('id',`${c.value}`)
+	lid.push({id:c.value.toLowerCase()})
+
 	var but=document.createElement('button');
 	but.setAttribute('class','x');
 	var bt=document.createTextNode('X');
+
 	but.appendChild(bt);
 	li.appendChild(but);
-	
 	ul.appendChild(li);
 }
 //deleting element
@@ -103,29 +62,34 @@ function del(e){
 	if (e.target.classList.contains('x')) {
 		if(confirm('are you sure')){
 			var list=e.target.parentElement;
-			ul.removeChild(list);
+			list.style.display="none"
+			var id=`${list.id}`
+			for( var i = 0; i < lid.length; i++)
+			if(lid[i].id==id){
+				lid.splice(i,1);
+			}
+			
 		}
 	} 
 }
 
-//filter
+//search paricular id
 var filter=document.getElementById('filter');
-filter.addEventListener('keydown',search)
-function search(e){
-
-	var text=e.target.value.toLowerCase();
-	var item=ul.getElementsByTagName('li');
-	//converting to aray
-	Array.from(item).forEach(function(i){
-		var itemName=i.firstChild.textContent;
-		if(itemName.toLowerCase().indexOf(text)!=-1){
-			i.style.display='inline-block';
+const search=()=>{
+	var text=filter.value.toLowerCase();
+	
+	//console.log(text,lid);
+	lid.forEach(element => {
+		var el=document.getElementById(`${element.id}`)
+		if(element.id.search(`${text}`)!=-1){
+			el.style.display='inline-block';
 		}
 		else{
-			i.style.display='none'
+			el.style.display='none'
 		}
-
 	});
+
+
 }
 
 	
